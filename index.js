a9114e6235b290c0d11ae956a4a9c4c69f77ed23
Nick Cartwright <nick@aslant.io>
2013-07-22 22:31:50 +0100

    expose path to expression-service

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.set('views', __dirname + '/views');
 
 
 app.use(express.static(__dirname))
+app.use('/expression-service', express.static(__dirname + '/tools-basis/expression-service'))
 app.use('./content-service', express.static(__dirname + '/content-service'))
 app.use('/web-client/', express.static(__dirname + '/tools-basis/web-client'))
 app.use('/web-client/host', express.static(__dirname + '/tools-basis/web-client/host'))
