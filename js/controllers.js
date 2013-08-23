ac1bca67362010f4e862c4d0632a8b27addd3d7c
Ollie Relph <ollie@relph.me>
2013-08-23 17:19:55 +0100

    Handle random values

--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,9 @@
   angular.module('toolApp.controllers', [])
     .controller('MenuController', function ($scope, $rootScope) {
 
+      $scope.MODE_OPTIONS = '0';
+      $scope.MODE_VALUES = '1';
+
       var $tool = $('#tool');
       $tool.load(function () {
         $scope.$apply(function() {
@@ -11,12 +14,54 @@
         });
       });
 
+      $scope.pickRandomValues = function (option) {
+        var min = option.minOptions;
+        var max = option.maxOptions;
+        var indices = [];
+        var indicesCount = Math.floor(Math.random() * max) + min;
+        while (indices.length < indicesCount) {
+          var i = Math.round(Math.random()*(option.options.length-1));
+          if (indices.indexOf(i) < 0) {
+            indices.push(i);
+            option.options[i]._selected = true;
+          }
+        }
+      };
+
+      $scope.toggleAny = function (option) {
+        $scope.deselectOptions(option.options);
+        $scope.pickRandomValues(option);
+      };
+
+      $scope.checkOptionValid = function (option) {
+        var count = 0;
+        angular.forEach(option._siblings, function(opt) {
+          if (opt._selected) {
+            count++;
+          }
+        });
+        if (count >= option._parent.minOptions && count <= option._parent.maxOptions) {
+          return;
+        }
+        option._selected = false; // undo the selection
+      };
+
+      $scope.optionMode = function (option) {
+        if (option.hasOwnProperty('options')) {
+          return $scope.MODE_OPTIONS;
+        }
+        if (option.hasOwnProperty('value')) {
+          return $scope.MODE_VALUES;
+        }
+      };
+
       $scope.mapSiblings = function (option) {
         if (angular.isUndefined(option)) {
           return [];
         }
         angular.forEach(option.options, function (opt) {
           opt._siblings = option.options;
+          opt._parent = option;
         });
         return option.options;
       };
